c6b2b1e27b0b7e86014cba038aa2dad357a90158
Justin Passmore <justinwpassmore@gmail.com>
2022-09-14 22:56:55 -0600

    Added debugger

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+debug = DebugToolbarExtension(app)
